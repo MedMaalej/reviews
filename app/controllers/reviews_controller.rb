74cb3194b09e3000d5b053094d7b71b2d6d08974
root <maalejmedti@gmail.com>
2016-04-29 10:44:35 +0100

    Repository's URL extracted

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,23 +5,24 @@ class ReviewsController < ApplicationController
 
   def index
      req = params['req'].blank? ? '' : params['req']
-     puts params['project_id']
      @projName = params['project_id']
      if req.eql?  '1'
         puts "in"
-        projectName = params['projectName']
-        proj = Project.where(name: projectName).pluck("id")
+        @projName = params['projectName']
+        proj = Project.where(name: @projName).pluck("id")
+        puts proj
         url = Repository.where(project_id: proj).pluck("url")
-        puts url
+        
         if url != nil  
            str = url.join('')
            str =str[0..str.length-6]
-           puts str
+           #puts str
            system "cd "+str + "&& git branch > branches.txt "
            @repo = str
-           redirect_to :back 
            
-        end   
+                     
+        end
+       
     end
   end
   
