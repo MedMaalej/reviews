d3082649ec580642000d3bede1a0ef3543bceda7
root <maalejmedti@gmail.com>
2016-05-25 13:56:57 +0100

    Module created ...

--- init.rb
+++ init.rb
@@ -3,8 +3,13 @@ Redmine::Plugin.register :reviews do
   author 'Mohamed Maalej - Squeezer software'
   description 'Code review plugin'
   version '0.0.1'
-  permission :reviewRequests, { :reviews => [:index] }, :public => true
-  permission :receiveRequests, { :reviews => [:receiveRequests] }, :public => true
-  menu :project_menu, :reviewRequests, { :controller => 'reviews', :action => 'index' }, :caption => 'Make review requests', :after => :activity, :param => :project_id
-  menu :project_menu, :receiveRequests, { :controller => 'reviews', :action => 'receiveRequests' }, :caption => 'Incoming review requests', :after => :reviewRequests , :param => :project_id
+  
+  project_module :reviews do
+     permission :reviewRequests, { :reviews => [:index] }, :public => true
+     permission :receiveRequests, { :reviews => [:receiveRequests] }, :public => true
+     menu :project_menu, :reviewRequests, { :controller => 'reviews', :action => 'index' }, :caption => 'Make review requests',
+      :after => :activity,  :param => :project_id
+     menu :project_menu, :receiveRequests, { :controller => 'reviews', :action => 'receiveRequests' }, :caption => 'Incoming review requests', 
+     :after => :reviewRequests , :param => :project_id
+  end
 end
