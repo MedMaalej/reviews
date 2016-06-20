17d3fef636c841cbdd47b293973eeefc26eca515
root <maalejmedti@gmail.com>
2016-06-20 13:09:33 +0100

    Permissions fixed

--- init.rb
+++ init.rb
@@ -4,9 +4,13 @@ Redmine::Plugin.register :reviews do
   author 'Mohamed Maalej - Squeezer software'
   description 'Code review plugin'
   version '0.0.1'
-     permission :reviewRequests, { :reviews => [:index] }, :public => true
-     permission :receiveRequests, { :reviews => [ :receiveRequests] }, :public => true
-     permission :showStats, { :reviews => [ :managerDashboard] }, :public => true
-     
+  project_module :reviews do
+     permission :send_review_requests,  :reviews => :index, :require => :member
+     permission :receive_review_requests, :reviews => :receiveRequests, :require => :member
+     permission :show_reviews_statistics,  :reviews => :managerDashboard, :require => :member
+     permission :manage_project_workflow, {}, :require => :member
+     permission :manage_repository, {}, :require => :member
+     permission :browse_repository, {}, :require => :member
+  end   
     
 end
